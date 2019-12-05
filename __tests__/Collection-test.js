448627d908ed5d1c83cacb653fd3bb239ab13290
kwillard95 <kwillard95@gmail.com>
2019-12-04 18:20:07 -0800

    Tests Collections functionality

---
+++ __tests__/Collection-test.js
@@ -0,0 +1,29 @@
+import 'jsdom-global/register';
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { mount, shallow } from 'enzyme';
+import Collections from '../src/Collections';
+import CollectionsStyle from '../src/styled-components/Collections-style'
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Collection', () => {
+    const adj = ['Social Media', 'News', 'Trending', 'Top 100'];
+
+    it('displays title of the section', () => {
+        const wrapper = shallow(<Collections />);
+        expect(wrapper.containsMatchingElement('Collections')).toBe(true);
+    })
+
+    it('renders adjectives to screen', () => {
+        const wrapper = shallow(<Collections adj={adj} />);
+        expect(wrapper.find(CollectionsStyle.CollectionContainer).children().length > 0).toBe(true)
+    })
+
+    it('renders all of the adjectives to the screen', () => {
+        const wrapper = shallow(<Collections adj={adj} />);
+        expect(wrapper.find(CollectionsStyle.CollectionContainer).children().length).toBe(4)
+    })
+
+
+})
